94b02275170c46631b30808fd72aa11607ea13b6
Liangdy <103975033+liangdy678@users.noreply.github.com>
2025-09-11 09:14:37 +0800

    Update download_hgmd_from_symbol.js

--- download_hgmd_from_symbol.js
+++ download_hgmd_from_symbol.js
@@ -1,29 +1,5 @@
-// 保存文件
-const save2file = (url, fileName) => {
-  const a = document.createElement("a");
-  a.href = url;
-  a.download = fileName; // 自定义文件名
-  a.style.display = "none";
-  document.body.appendChild(a);
-  a.click();
-  document.body.removeChild(a);
-};
-
-// 重试函数
-const retry = async (fn, args, max_retry = 3) => {
-  let err = null;
-  for (let i = 0; i < max_retry; i++) {
-    err = await fn(args);
-    if (err) {
-      continue;
-    }
-    // 成功,则跳出循环
-    break;
-  }
-  return err;
-};
-
 // 请求函数
+//host: https://my.qiagendigitalinsights.com/
 const request = async (body) => {
   const url = "/bbp/view/hgmd/pro/all.php";
   try {
@@ -47,9 +23,20 @@ const save_to_file = async (params, file) => {
   const [blob, err] = await request(params);
 
   if (blob) {
+    // 创建Blob对象URL
     const url = URL.createObjectURL(blob);
-    save2file(url, file); //使用a标签方法 + blob url
-    URL.revokeObjectURL(url); // 释放内存
+
+    // 保存文件
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = file; // 自定义文件名
+    a.style.display = "none";
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+
+    // 释放内存
+    URL.revokeObjectURL(url);
   }
   return err;
 };
@@ -57,7 +44,7 @@ const save_to_file = async (params, file) => {
 // 下载基因变异
 const download_gene_mutations = async (gene) => {
   const params = `gene=${gene}&sorted=location&database=Get+all+mutations`;
-  const file = `${gene}.html`;
+  const file = `${gene}.mutaiton.html`;
   const err = await save_to_file(params, file);
   return err;
 };
@@ -65,11 +52,25 @@ const download_gene_mutations = async (gene) => {
 // 下载基因总结
 const download_gene_summary = async (gene) => {
   const params = `gene=${gene}&sorted=location&database=Gene+summary`;
-  const file = `${gene}.html`;
+  const file = `${gene}.summary.html`;
   const err = await save_to_file(params, file);
   return err;
 };
 
+// 重试函数
+const retry = async (fn, args, max_retry = 3) => {
+  let err = null;
+  for (let i = 0; i < max_retry; i++) {
+    err = await fn(args);
+    if (err) {
+      continue;
+    }
+    break;
+  }
+  return err;
+};
+
+// 通用函数
 const run_common = async (fn, symbols) => {
   const start = performance.now();
   const faileds = [];
@@ -82,9 +83,7 @@ const run_common = async (fn, symbols) => {
   }
 
   const end = performance.now();
-  console.log(
-    `下载${symbols.length}个基因总结耗时${Math.ceil((end - start) / 1000)}秒。`
-  );
+  console.log(`耗时：${Math.ceil((end - start) / 1000)}秒。`);
 
   return faileds;
 };
@@ -107,10 +106,19 @@ const run_summary = async (symbols) => {
   }
 };
 
+function main(symbols) {
+  if (symbols?.length <= 0) {
+    return;
+  }
+  run_mutations(symbols);
+  run_summary(symbols);
+}
+
 /******************执行段******************/
+// 待下载基因列表,每天累计最多900个！,每月累计最多9000个！，不然会封ip
 
-const symbols = [];
+// grep -vxF -f file1 file2  file2中不存在于file1中的行
 
-run_mutations(symbols);
+const symbols = ["PITX1-AS1"];
 
-run_summary(symbols);
+main(symbols);
